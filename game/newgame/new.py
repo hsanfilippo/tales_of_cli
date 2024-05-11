541edeae2e27ef25dd1ff773af85fe2953632863
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-05-12 05:47:25 +0900

    makeSavegame function created

--- game/newgame/new.py
+++ game/newgame/new.py
@@ -1,8 +1,19 @@
+from set_char_info import checkCharClass, makeCharTemp, checkCharName, makeSavegame
+
 print("Create your character:")
 print("Name: ")
 char_name = input()
+checkCharName(char_name)
 
-print(f"Pick a class: ")
+print("Pick a class: ")
 print("1 - Warrior | 2 - Mage | 3 - Archer")
 char_class = input()
+checkCharClass(char_class)
+makeCharTemp(char_name, char_class)
 
+# print("Deseja criar: ")
+# print(f"[{char_name} - {checkCharClass(char_class)}]?")
+# create = input()
+# checkYesNo(create, makeSavegame(f'{char_name}_temp.csv', f'{char_name}'))
+
+makeSavegame(f'{char_name}_temp.csv', f'{char_name}')
--- game/newgame/set_char_info.py
+++ game/newgame/set_char_info.py
@@ -1,10 +1,11 @@
-char_name = 'Nezuxu'
-char_class = 'Warrior'
+import shutil
+import os
 
 def checkCharName(c_name: str) -> str:
-    if c_name.__len__() <= 1:
+    while c_name.__len__() <= 1:
         print('Nome muito curto. (minimo 3 caracteres)')
-    else:
+        c_name = input()
+    if c_name.__len__() >= 3:
         print('Nome salvo')
         return c_name
 
@@ -13,20 +14,15 @@ def checkCharClass(c_class: str) -> str:
 
     while is_ok == False:
         if c_class == '1':
-            c_class = 'Warrior'
-            return c_class
+            return 'Warrior'
 
         elif c_class == '2':
-            c_class = 'Mage'
             print(f'{c_class} não disponível na versão demo. Mudando p/ Warrior.')
-            c_class = 'Warrior'
-            return c_class
+            return 'Warrior' # Futuro mage
 
         elif c_class == '3':
-            c_class = 'Archer'
             print(f'{c_class} não disponível na versão demo. Mudando p/ Warrior.')
-            c_class = 'Warrior'
-            return c_class
+            return 'Warrior' # Futuro archer
 
         else:
             print('Opção inválida. Escolha uma das opções abaixo:')
@@ -35,13 +31,24 @@ def checkCharClass(c_class: str) -> str:
             c_class = input()
 
 
-def setCharInfo(c_name: str, c_class: str):
-    print(f'{c_name}, {c_class} criado com sucesso! Bom jogo!')
-    with open(f'{char_name}.txt', 'w', encoding='utf8') as fp:
-        fp.write('char_name,char_class,lvl,exp,hp,atk,def,m_atk,m_def\n')
-        fp.write(f'{char_name},{char_class},1,0,100,5,3,0,1')
+def makeCharTemp(c_name: str, c_class: str):
 
+    with open(f'{c_name}_temp.csv', 'w', encoding='utf8') as fp:
+        fp.write('char_name,char_class,lvl,exp,hp,atk,def,m_atk,m_def,save_state\n')
+        fp.write(f'{c_name},{checkCharClass(c_class)},1,0,100,5,3,0,1,fase 1')
+    print(f'[{c_name} - {checkCharClass(c_class)} - lvl. 1] criado com sucesso. Bom jogo!')
 
-# checkCharName(char_name)
-# checkCharClass(char_class)
-setCharInfo(checkCharName(char_name), checkCharClass(char_class))
+def makeSavegame(temp_char_name: str, c_name: str):
+    shutil.copyfile(src=f'../newgame/{temp_char_name}', dst=f'../save_game/{c_name}.csv')
+    os.remove(f'../newgame/{temp_char_name}')
+
+# def checkYesNo(input_str: str, func_name):
+    
+#     while input_str.lower() not in ['y', 's', 'n']:
+#         if input_str.lower() == 's':
+#             func_name
+#         elif input_str.lower() == 'n':
+#             print('Voltar p/ criação de personagem')
+#         else:
+#             print('Digite uma opção válida (s ou n)')
+#             input_str = input()
